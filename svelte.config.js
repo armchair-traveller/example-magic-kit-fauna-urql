c5c3850cb75e17d49f89680dc643f3c722dbcd3f
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-12 04:49:16 -0700

    simplify config (default init)

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    vite: { optimizeDeps: { exclude: ['@urql/svelte'] } },
     ssr: false,
   },
 
